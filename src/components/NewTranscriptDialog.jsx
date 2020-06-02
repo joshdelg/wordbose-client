78150bd951e760ded05f8aac6cde266ce51ddfb3
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2020-06-01 23:06:23 -0700

    Added a name field into the new transcript dialog box, and generated the transcriptId client side. This allows for the user to specify metadata before uploading the file. Overall, just a refactor to couple with the backend refactoring. :triumph:

--- src/components/NewTranscriptDialog.jsx
+++ src/components/NewTranscriptDialog.jsx
@@ -1,23 +1,42 @@
 import React, { useState } from "react";
-import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, Input} from "@material-ui/core";
-import { Storage } from "aws-amplify";
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, Input, TextField} from "@material-ui/core";
+import { API, Storage } from "aws-amplify";
+import { v4 as uuidv4 } from "uuid";
+import moment from "moment";
 
 
 function NewTranscriptDialog(props) {
 
   const [file, setFile] = useState("");
+  const [transcriptName, setTranscriptName] = useState("");
 
   const formatFileName = (name) => {
     return name.substring(0, name.lastIndexOf('.')).replace(/\W/g, "") + name.substring(name.lastIndexOf('.'));
   };
 
   const onUpload = async () => {
+  
+    const transcriptId = uuidv4();
+    const extension = file.name.substring(file.name.lastIndexOf('.'));
+
+    console.log(transcriptId);
     console.log(file);
+
+    // Call create route
+    API.post("transcripts", '/transcript', {
+      body: {
+        transcriptId: transcriptId,
+        transcriptName: transcriptName,
+        date: moment(),
+        fileName: file.name
+      }
+    });
     
     try {
-      const stored = await Storage.vault.put(formatFileName(file.name), file, {
+      const stored = await Storage.vault.put(transcriptId + extension, file, {
         contentType: file.type
       });
+
       console.log(stored);
       props.onClose();
     } catch(e) {
@@ -30,6 +49,7 @@ function NewTranscriptDialog(props) {
       <DialogTitle>New Transcript</DialogTitle>
         <DialogContent>
           <DialogContentText>Upload your file here</DialogContentText>
+          <TextField label="Transcript Name" value={transcriptName} onChange={(e) => setTranscriptName(e.target.value)}/>
           <Input type="file" onChange={(e) => setFile(e.target.files[0])}/>
         </DialogContent>
         <DialogActions>
