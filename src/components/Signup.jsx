409f32971feae3e2d248e7a5773c600baf35cc07
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2020-08-19 21:58:01 -0700

    Added state to disable signup and confirm buttons when form is processing

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -33,17 +33,28 @@ function Signup() {
   const [password, setPassword] = useState("");
   const [code, setCode] = useState("");
   const [step, setStep] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
   const classes = useStyles();
   let history = useHistory();
 
   const { dispatch } = useContext(AuthContext);
 
+  const validateForm = () => {
+    // Basic form validation
+    if(name && email && password) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
 
     // Basic form valdiation
-    if(name && email && password) {
+    if(validateForm()) {
       try {
         const user = await Auth.signUp({username: email, password: password});
         alert("Sign Up Successful! Check your email for a confirmation code");
@@ -59,6 +70,8 @@ function Signup() {
         }
       }
     }
+
+    setIsLoading(false);
   };
 
   const resendCode = async() => {
@@ -68,6 +81,7 @@ function Signup() {
 
   const submitCode = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
 
     try {
       // Use confirmation code
@@ -96,6 +110,7 @@ function Signup() {
       } else {
         console.log(e);
         alert(e.message);
+        setIsLoading(false);
       }
     }
   };
@@ -106,7 +121,7 @@ function Signup() {
         <TextField className={classes.formItem} label="Full Name" value={name} onChange={(e) => setName(e.target.value)} /> 
         <TextField className={classes.formItem} label="Email" value={email} helperText="Emails are not case sensitive" onChange={(e) => setEmail(e.target.value.toLowerCase())}/>
         <TextField className={classes.formItem} label="Password" type="password" value={password} helperText="Passwords must contain upper and lowercase letters, a number, and a symbol" onChange={(e) => setPassword(e.target.value)}/>
-        <Button type="submit" className={classes.formItem} variant="contained" color="primary">Submit</Button>
+        <Button type="submit" className={classes.formItem} variant="contained" color="primary" disabled={!validateForm() || isLoading}>Submit</Button>
         <Typography className={classes.formItem} variant="body1">
           By creating an account you agree to Wordbose's&nbsp;
           <Link className={classes.link} variant="inherit" color="secondary" href="http://wordbose.com/terms.html" target="_blank" rel="noopener noreferrer">Terms and Conditions</Link>,&nbsp;
@@ -121,8 +136,8 @@ function Signup() {
     return (
       <form className={classes.flexForm} onSubmit={submitCode}>
         <TextField className={classes.formItem} label="Confirmation Code" value={code} onChange={(e) => setCode(e.target.value)}/>
-        <Button type="submit" className={classes.formItem} variant="contained" color="primary">Submit Code</Button>
-        <Button className={classes.formItem} variant="text" color="secondary" onClick={resendCode}>Resend Confimation Code</Button>
+        <Button type="submit" className={classes.formItem} variant="contained" color="primary" disabled={!code || isLoading}>Submit Code</Button>
+        <Button className={classes.formItem} variant="text" color="secondary" onClick={resendCode} disabled={isLoading}>Resend Confimation Code</Button>
       </form>
     );
   };
