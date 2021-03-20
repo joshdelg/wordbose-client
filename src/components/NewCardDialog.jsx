bfdf415314a5f300c4ae47835f92398257423846
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2021-03-19 22:54:55 -0700

    Linked up to backend to confirm setup intents

--- src/components/NewCardDialog.jsx
+++ src/components/NewCardDialog.jsx
@@ -1,11 +1,16 @@
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button, useTheme, useMediaQuery } from "@material-ui/core";
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import React, { useState } from "react";
+import { API } from "aws-amplify";
 
 function NewCardDialog(props) {
-
     const theme = useTheme();
     const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const [loading, setLoading] = useState(false);
+
+    const stripe = useStripe();
+    const elements = useElements();
 
     const cardStyle = {
         style: {
@@ -25,6 +30,34 @@ function NewCardDialog(props) {
         }
     };
 
+    const handleAddCard = async() => {
+        if(!stripe || !elements) {
+            return;
+        }
+
+        try {
+            setLoading(true);
+            const response = await API.post("transcripts", "/createSetupIntent");
+
+            const result = await stripe.confirmCardSetup(response.clientSecret, {
+                payment_method: {
+                    card: elements.getElement(CardElement)
+                }
+            });
+
+            if(result.error) {
+                alert("There was a problem with stripe :(");
+            } else {
+                alert("Everything worked!");
+                console.log(result.setupIntent.payment_method);
+            }
+        } catch(e) {
+            alert("Something failed with the API request l boa :(");
+        }
+        setLoading(false);
+        props.handleClose();
+    };
+
     return (
         <Dialog open={props.isOpen} onClose={props.handleClose} fullWidth={true}>
             <DialogTitle>Add New Card</DialogTitle>
@@ -35,7 +68,8 @@ function NewCardDialog(props) {
                 <CardElement options={cardStyle} />
             </DialogContent>
             <DialogActions>
-                <Button onClick={props.handleClose} color="primary" variant="contained">Cancel</Button>
+                <Button onClick={props.handleClose} color="primary" variant="contained" disabled={loading}>Cancel</Button>
+                <Button onClick={handleAddCard} color="primary" variant="contained" disabled={!stripe || loading}>Add</Button>
             </DialogActions>
         </Dialog>
     );
